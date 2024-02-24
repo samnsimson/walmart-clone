41507253e9cade08406bc247fb45831cf1264745
samnsimson <samnishanth01@gmail.com>
2024-02-23 19:57:35 -0500

    changed ts file

--- lib/hooks/queries.ts
+++ lib/hooks/queries.ts
@@ -9,3 +9,5 @@ interface Props {
 export const useFetchProducts = (props: Props) => {
   return useQuery({ queryKey: ["product", props], queryFn: () => axios.get(`/api/products?category=${props.id}`), initialData: props });
 };
+
+export const useFetchCategories = (props: Props) => {};
