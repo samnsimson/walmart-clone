5a1e929e20613d7059dcb9fd18d1ec0b07ef5b53
samnsimson <samnishanth01@gmail.com>
2024-03-06 00:30:25 -0500

    spell check

---
+++ components/addToCartButton/index.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { MinusIcon, PlusIcon } from "lucide-react";
+import { Button } from "../ui/button";
+import { FC } from "react";
+import useStore from "@/lib/store";
+
+interface AddToCartButtonProps {
+    productId: string;
+    buttonText?: string;
+}
+
+const AddToCartButton: FC<AddToCartButtonProps> = ({ productId: id, buttonText }) => {
+    const { cart, addToCart, removeFromCart } = useStore((state) => state);
+    const item = cart.find((item) => item.id === id);
+
+    if (!item) {
+        return (
+            <Button variant="outline" className="btn-default" size="sm" onClick={() => addToCart({ id, quantity: 1 })}>
+                <PlusIcon strokeWidth={1} color="black" size={24} /> <span>{buttonText ?? "Add"}</span>
+            </Button>
+        );
+    }
+
+    return (
+        <div className="flex items-center justify-between">
+            <Button variant="outline" size="sm" className="btn-default rounded-full" onClick={() => removeFromCart(id)}>
+                <MinusIcon size={24} strokeWidth={1} />
+            </Button>
+            <div className="font-bold">{item.quantity}</div>
+            <Button variant="default" size="sm" className="rounded-full bg-gray-400 text-white" onClick={() => addToCart({ id, quantity: 1 })}>
+                <PlusIcon size={24} strokeWidth={1} />
+            </Button>
+        </div>
+    );
+};
+export default AddToCartButton;
--- components/productCard/index.tsx
+++ components/productCard/index.tsx
@@ -3,52 +3,52 @@ import { Card, CardContent, CardFooter, CardHeader } from "../ui/card";
 import Image from "next/image";
 import { AspectRatio } from "../ui/aspect-ratio";
 import Link from "next/link";
-import AddToCartButton from "../addToCardButton";
+import AddToCartButton from "../addToCartButton";
 import { FavouritesButton } from "../favouritesButton";
 
 interface BaseProductCardProps {
-  productId: string;
-  image: string;
-  title: string;
-  slug: string;
-  price: {
-    retail: number;
-    sale: number;
-  };
+    productId: string;
+    image: string;
+    title: string;
+    slug: string;
+    price: {
+        retail: number;
+        sale: number;
+    };
 }
 
 type CompactFormat = {
-  format: "compact";
+    format: "compact";
 };
 
 type DetailedFormat = {
-  format: "detailed";
-  rating: number;
+    format: "detailed";
+    rating: number;
 };
 
 type ProductCardProps = BaseProductCardProps & (CompactFormat | DetailedFormat);
 
 export const ProductCard: FC<ProductCardProps> = ({ productId, title, image, price: { retail, sale }, ...props }) => {
-  return (
-    <Card {...props} className="overflow-hidden border-0 p-1 shadow-none">
-      <CardHeader className="relative overflow-hidden rounded-lg p-0">
-        <AspectRatio ratio={1 / 1}>
-          <Image alt={title} fill src={image} sizes="(max-width: 768px) 100vw, 33vw" />
-        </AspectRatio>
-        <FavouritesButton productId={productId} />
-      </CardHeader>
-      <Link href={{ pathname: `/shop/product/${productId}` }}>
-        <CardContent className="prose-base px-0 py-4">
-          <h3 className="m-0">
-            {sale && <span className="font-bold text-success">Now ${sale}</span>}{" "}
-            {retail && <span className="text-base text-zinc-500 line-through">${retail}</span>}
-          </h3>
-          <p className="m-0 line-clamp-2 leading-snug">{title}</p>
-        </CardContent>
-      </Link>
-      <CardFooter className="block p-0">
-        <AddToCartButton productId={productId} />
-      </CardFooter>
-    </Card>
-  );
+    return (
+        <Card {...props} className="overflow-hidden border-0 p-1 shadow-none">
+            <CardHeader className="relative overflow-hidden rounded-lg p-0">
+                <AspectRatio ratio={1 / 1}>
+                    <Image alt={title} fill src={image} sizes="(max-width: 768px) 100vw, 33vw" />
+                </AspectRatio>
+                <FavouritesButton productId={productId} />
+            </CardHeader>
+            <Link href={{ pathname: `/shop/product/${productId}` }}>
+                <CardContent className="prose-base px-0 py-4">
+                    <h3 className="m-0">
+                        {sale && <span className="font-bold text-success">Now ${sale}</span>}{" "}
+                        {retail && <span className="text-base text-zinc-500 line-through">${retail}</span>}
+                    </h3>
+                    <p className="m-0 line-clamp-2 leading-snug">{title}</p>
+                </CardContent>
+            </Link>
+            <CardFooter className="block p-0">
+                <AddToCartButton productId={productId} />
+            </CardFooter>
+        </Card>
+    );
 };
